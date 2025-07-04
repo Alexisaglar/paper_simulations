de69b4f369fa5cdfb80f35c9e541e3fa442db2be
smartgrid <alexisaglar@gmail.com>
2025-07-04 13:46:29 +0100

    added plots for chapter

--- chapter_3_simulations.py
+++ chapter_3_simulations.py
@@ -114,39 +114,57 @@ def power_generation_pv(irradiance, temperature, season):
     return pv_power_sd_decrease, pv_power_epv, pv_power_epv_increased, total_percentage_output_technology
 
 def plot_heatmap(irradiance_grid: np.array, temperature_grid: np.array, pv_power: np.array, name: str) -> None:
-    fig, ax = plt.subplots(figsize=(10, 8))
+    plt.rcParams.update({
+        'font.size': 18,
+        'axes.labelsize': 20,
+        'axes.titlesize': 24,
+        'xtick.labelsize': 16,
+        'ytick.labelsize': 16,
+        'legend.fontsize': 18,
+    })
+
+    fig, ax = plt.subplots(figsize=(10, 4))
 
     levels = np.linspace(np.min(pv_power), np.max(pv_power), 15)
+    levels = np.round(levels, 2)
 
     contour = ax.contourf(irradiance_grid, temperature_grid, pv_power, levels=levels, cmap='YlGn')
 
     cbar = fig.colorbar(contour)
-    cbar.set_label('Power Difference (u_epv - u_si) in Watts', rotation=270, labelpad=20)
+    cbar.set_label('Power Difference (kW)', rotation=270, labelpad=30)
 
     ax.set_xlabel('Irradiance (W/m²)', labelpad=15)
     ax.set_ylabel('Temperature (°C)', labelpad=15)
-    ax.set_title(f'{name}', pad=20)
+    # ax.set_title(f'{name}', pad=20)
     ax.grid(True, linestyle='--', alpha=0.6)
     plt.savefig(f'chapter_3_images/{name}.png', format='png', dpi=300, bbox_inches='tight')
 
     # plt.show()
 
 def plot_3d(irradiance_grid: np.array, temperature_grid: np.array, pv_power: np.array, name: str) -> None:
+    plt.rcParams.update({
+        'font.size': 18,
+        'axes.labelsize': 20,
+        'axes.titlesize': 24,
+        'xtick.labelsize': 16,
+        'ytick.labelsize': 16,
+        'legend.fontsize': 18,
+    })
     fig = plt.figure(figsize=(14, 10))
     
     ax = fig.add_subplot(111, projection='3d')
 
     surf = ax.plot_surface(irradiance_grid, temperature_grid, pv_power,
-                           cmap="YlGn", linewidth=0, antialiased=False)
+                           cmap="YlOrBr", linewidth=0, antialiased=False)
     ax.set_xlabel('Irradiance (W/m²)', labelpad=15)
     ax.set_ylabel('Temperature (°C)', labelpad=15)
-    ax.set_title(f'{name}', pad=20, fontsize=16)
-    ax.view_init(elev=30, azim=-60)
+    # ax.set_title(f'{name}', pad=20, fontsize=16)
+    ax.view_init(elev=5, azim=-40)
+    cbar = fig.colorbar(contour, shrink=0.6, aspect=10, pad=0.1)
+    cbar.set_label('Power Difference (kW)', rotation=270, labelpad=30)
     plt.savefig(f'chapter_3_images/{name}.png', format='png', dpi=300, bbox_inches='tight')
-
-    fig.colorbar(surf, shrink=0.6, aspect=10, pad=0.1)
-
     # plt.show()
+
 
 
 if __name__ == '__main__':
@@ -162,12 +180,23 @@ if __name__ == '__main__':
             power_si[i, j] = single_diode(irradiance_range[i], temperature_range[j]) * (PV_PARAMETERS['series_cell'] * PV_PARAMETERS['parallel_cell'])
             power_epv[i, j] = power_si[i, j] * delta_mu(irradiance_range[i], temperature_range[j], 'epv')
 
+    # power_epv = np.round((power_epv/1000), 2)
+    # power_si = np.round((power_si/1000), 2)
+    # print(power_epv)
+    # print(power_si)
+    #
+    # delta_p = power_epv - power_si
+    # delta_p = np.round(delta_p, 2)
+    # print(delta_p)
+    #
+    power_epv = power_epv/1000
+    power_si = power_si/1000
     delta_p = power_epv - power_si
-
+    
     plot_heatmap(irradiance_grid,  temperature_grid, delta_p, "PV generation difference heatmap")
     plot_heatmap(irradiance_grid,  temperature_grid, power_si, "pv generation Silicon PV heatmap")
     plot_heatmap(irradiance_grid,  temperature_grid, power_epv, "pv generation EPV")
 
     plot_3d(irradiance_grid,  temperature_grid, delta_p, "PV generation difference 3d")
-    plot_3d(irradiance_grid,  temperature_grid, power_si, "PV Silicon 3d")
-    plot_3d(irradiance_grid,  temperature_grid, power_epv, "PV EPV 3d")
+    # plot_3d(irradiance_grid,  temperature_grid, power_si, "PV Silicon 3d")
+    # plot_3d(irradiance_grid,  temperature_grid, power_epv, "PV EPV 3d")
